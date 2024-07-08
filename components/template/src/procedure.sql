207bf5f624e52d329fb723aad19b3fd608630c48
volaya <volayaf@gmail.com>
2024-07-08 08:10:52 +0200

    minor fixes

--- components/template/src/procedure.sql
+++ components/template/src/procedure.sql
@@ -16,16 +16,16 @@ BEGIN
         CREATE TABLE IF NOT EXISTS ''' || output_table || '''
         OPTIONS (expiration_timestamp = TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 30 DAY))
         AS SELECT *, \'''' || value || '''\' AS fixed_value_col
-        FROM ''' || input_table ;
+        FROM ''' || input_table;
     END IF;
 END;
 
 
 
-/*
+
 -- This is a template for creating a SnowFlake procedure.
 ---------------------------------------------------------
-
+/*
 CREATE OR REPLACE PROCEDURE @@workflows_temp@@.ADD_FIXED_VALUE_COLUMN(input_table STRING, value STRING, output_table STRING, dry_run BOOLEAN)
 RETURNS VARCHAR
 LANGUAGE SQL
